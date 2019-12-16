cd31da8fd8325bdce97e836f1c89c2210d47a3ff
ChidambaramPG <chidambarampg@gmail.com>
2019-12-16 18:57:16 +0530

    deploy

---
+++ precache-manifest.a631179610971c6a886a9a4e9cc6cdb2.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3d116b1d48e4218d3d",
+    "url": "/bcm-datacollection/css/app.7d8c407b.css"
+  },
+  {
+    "revision": "1428738fab950b450bea",
+    "url": "/bcm-datacollection/css/chunk-vendors.af3b3bc8.css"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/bcm-datacollection/fonts/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/bcm-datacollection/fonts/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/bcm-datacollection/fonts/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/bcm-datacollection/fonts/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/bcm-datacollection/fonts/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/bcm-datacollection/fonts/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/bcm-datacollection/fonts/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/bcm-datacollection/fonts/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/bcm-datacollection/fonts/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/bcm-datacollection/fonts/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/bcm-datacollection/fonts/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/bcm-datacollection/fonts/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "3455f690f6da0f7c5dda8b7d9f42e347",
+    "url": "/bcm-datacollection/img/card.3455f690.jpg"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/bcm-datacollection/img/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/bcm-datacollection/img/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/bcm-datacollection/img/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "8a5a8451b05c3d74da16133ebb621dc8",
+    "url": "/bcm-datacollection/img/id-card.8a5a8451.png"
+  },
+  {
+    "revision": "dc1ef7c99b5f4ab7c8d86c14bd9099e5",
+    "url": "/bcm-datacollection/index.html"
+  },
+  {
+    "revision": "0d3d116b1d48e4218d3d",
+    "url": "/bcm-datacollection/js/app.74a38ae5.js"
+  },
+  {
+    "revision": "1428738fab950b450bea",
+    "url": "/bcm-datacollection/js/chunk-vendors.65e9a575.js"
+  },
+  {
+    "revision": "3ae0a0457fdd5506712cb4fbefc71a70",
+    "url": "/bcm-datacollection/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bcm-datacollection/robots.txt"
+  }
+]);
